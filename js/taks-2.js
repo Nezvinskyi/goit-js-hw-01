06f32a4309ad494b4f8e4ae712135d8230242cd8
Dmitry Nezvinskyi <Nezvinskyi@users.noreply.github.com>
2020-11-25 01:02:54 +0100

    Create taks-2.js

---
+++ js/taks-2.js
@@ -0,0 +1,19 @@
+// const variable
+
+const total = 100;
+let ordered = 50;
+
+if (ordered > total) {
+  console.log ('На складе недостаточно товаров!');
+} else {
+  console.log ('Заказ оформлен, с вами свяжется менеджер')
+}
+
+ordered = 20;
+console.log(ordered > total ? 'На складе недостаточно товаров!' : 'Заказ оформлен, с вами свяжется менеджер');
+
+ordered = 100;
+console.log(ordered > total ? 'На складе недостаточно товаров!' : 'Заказ оформлен, с вами свяжется менеджер');
+
+ordered = 130;
+console.log(ordered > total ? 'На складе недостаточно товаров!' : 'Заказ оформлен, с вами свяжется менеджер');
